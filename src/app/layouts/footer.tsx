7e9949a8eec9d38c314081f04f3206e389490d65
Josh Reeter <joshreeter@joshs-mbp-2.attlocal.net>
2024-01-20 22:34:14 -0600

    Add footer.

--- src/app/layouts/footer.tsx
+++ src/app/layouts/footer.tsx
@@ -1,7 +1,33 @@
 import { Box } from '@mui/material';
 
 const Footer = () => {
-  return <Box>This is a footer.</Box>;
+  const footerStyle: React.CSSProperties = {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    position: 'fixed',
+    bottom: '0',
+  };
+
+  return (
+    <Box style={footerStyle}>
+      <Box
+        style={{
+          width: '100%',
+          height: '100px',
+          display: 'flex',
+          justifyContent: 'left',
+          backgroundColor: 'white',
+          padding: '5px',
+          borderRadius: '25px',
+          maxHeight: '50px',
+        }}
+      >
+        Footer
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
